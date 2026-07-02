d88d27f509ae21fae842b60e1e65ff8fbf32285f
ayan65ty <s.ayantayo2003@gmail.com>
2026-07-02 16:53:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,17 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+    @app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
 
 
 @app.route("/pdf")
